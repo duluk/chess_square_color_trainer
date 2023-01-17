da0e20b46318c4f575ba5181734b988c34a76cf2
Duluk <jab3@korazin.net>
2023-01-17 18:59:05 -0500

    Repeat the same coordinate upon invalid answer
    - If the answer given is invalid, ask for the same coordinate until a
      valid answer is provided.
    - Also reworked the color algorithm to use bitwise math...because.

--- chess_square_color_trainer.cpp
+++ chess_square_color_trainer.cpp
@@ -4,15 +4,15 @@
 #include <cstdlib>
 #include <ctime>
 
-// The following two functions were inspired by ChatGPT-generated functions for
-// the Ruby version of this.
+// More succinct. The basic idea is that if the values of the coordinates add
+// up to an even number, then the square is dark. If we see the letters as
+// (a=1,b=2,c=3,etc), then we can simply add the two together if it is even
+// it's dark. The bitwise AND with 1 will evaluate to 1 for even and 0 for odd.
+// I leave that as an exercise for the reader.
 std::string
 chess_square_color(std::string coord)
 {
-  int x = (coord[0] - 'a') + 1;
-  int y = coord[1] - '1';
-
-  return (x + y) % 2 == 0 ? "light" : "dark";
+  return (coord[0] + coord[1]) & 1 ? "light" : "dark";
 }
 
 std::string
@@ -38,7 +38,10 @@ int main(void)
 
   for (;;)
   {
-    coord = random_chess_coordinate();
+    if (coord == "")
+    {
+      coord = random_chess_coordinate();
+    }
 
     std::cout << "What color is " << coord << "? ";
     std::cin >> guess;
@@ -49,7 +52,8 @@ int main(void)
       break;
     else if (guess != "light" && guess != "dark")
     {
-      std::cout << "Invalid input. Must be either light or dark." << std::endl;
+      std::cout << "Invalid input. Must be either light or dark." << std::endl << std::endl;
+      continue;
     }
     else if (guess == chess_square_color(coord))
     {
@@ -59,6 +63,8 @@ int main(void)
     {
       std::cout << "Wrong you fool!" << std::endl;
     }
+
+    coord = "";
 
     // Newline for separation/cleanliness
     std::cout << std::endl;
